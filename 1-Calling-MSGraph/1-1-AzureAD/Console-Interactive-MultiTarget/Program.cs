78bc65460001457038574fcd856b8969b0996c68
Shama <v-shln@microsoft.com>
2020-05-29 13:01:00 -0700

    Microsoft graph implementation

--- 1-Calling-MSGraph/1-1-AzureAD/Console-Interactive-MultiTarget/Program.cs
+++ 1-Calling-MSGraph/1-1-AzureAD/Console-Interactive-MultiTarget/Program.cs
@@ -31,60 +31,29 @@ namespace Console_Interactive_MultiTarget
             // Building a public client application
             var app = PublicClientApplicationBuilder.Create(appConfiguration.ClientId)
                                                     .WithAuthority(_authority)
-                                                    .WithRedirectUri(appConfiguration.RedirectUri)
+                                                    .WithDefaultRedirectUri()
                                                     .Build();
 
             string[] scopes = new[] { "user.read" };
-            AuthenticationResult result;
 
-            try
-            {
-                var accounts = await app.GetAccountsAsync();
+            //Setting the value of properties
+            SingletonGraphServiceClient.Instance.App = app;
+            SingletonGraphServiceClient.Instance.Scopes = scopes;
 
-                // Try to acquire an access token from the cache. If an interaction is required, 
-                // MsalUiRequiredException will be thrown.
-                result = await app.AcquireTokenSilent(scopes, accounts.FirstOrDefault())
-                            .ExecuteAsync();
-            }
-            catch (MsalUiRequiredException)
-            {
-                // Acquiring an access token interactively. MSAL will cache it so we can use AcquireTokenSilent
-                // on future calls.
-                result = await app.AcquireTokenInteractive(scopes)
-                            .ExecuteAsync();
-            }
-
-            string graphApiUrl = configuration.GetValue<string>("GraphApiUrl");
-            // Instantiating GraphServiceClient and using the access token acquired above.
-            var graphClient = GetGraphServiceClient(result.AccessToken, graphApiUrl);
+            //Get graph service client
+            var graphClient = SingletonGraphServiceClient.GraphClient;
 
             // Calling the /me endpoint
             var me = await graphClient.Me.Request().GetAsync();
 
             // Printing the results
             Console.Write(Environment.NewLine);
-            Console.WriteLine($"Hello {result.Account.Username}");
             Console.Write(Environment.NewLine);
             Console.WriteLine("-------- GRAPH RESULT --------");
             Console.Write(Environment.NewLine);
             Console.WriteLine($"Id: {me.Id}");
             Console.WriteLine($"Display Name: {me.DisplayName}");
             Console.WriteLine($"Email: {me.Mail}");
-        }
-
-        private static GraphServiceClient GetGraphServiceClient(string accessToken, string graphApiUrl)
-        {
-            GraphServiceClient graphServiceClient = new GraphServiceClient(graphApiUrl,
-                                                                 new DelegateAuthenticationProvider(
-                                                                     async (requestMessage) =>
-                                                                     {
-                                                                         await Task.Run(() =>
-                                                                         {
-                                                                             requestMessage.Headers.Authorization = new AuthenticationHeaderValue("bearer", accessToken);
-                                                                         });
-                                                                     }));
-
-            return graphServiceClient;
         }
     }
 }
---
+++ 1-Calling-MSGraph/1-1-AzureAD/Console-Interactive-MultiTarget/SingletonGraphServiceClient.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.Identity.Client;
+using Microsoft.Graph;
+using System.Linq;
+using System.Net.Http.Headers;
+
+namespace Console_Interactive_MultiTarget
+{
+    public sealed class SingletonGraphServiceClient
+    {
+        private SingletonGraphServiceClient()
+        {
+            GraphClient = GetSingletonGraphServiceClient();
+        }
+        private static SingletonGraphServiceClient instance = null;
+        public static SingletonGraphServiceClient Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new SingletonGraphServiceClient();
+                }
+                return instance;
+            }
+        }
+        public IPublicClientApplication App { get; set; }
+        public string[] Scopes { get; set; }
+        public static GraphServiceClient GraphClient { get; set; }
+
+        /// <summary>
+        /// Method to acquire the access token and instantiate the graph service client.
+        /// </summary>
+        /// <returns>GraphServiceClient</returns>
+        private GraphServiceClient GetSingletonGraphServiceClient()
+        {
+            GraphServiceClient graph = new GraphServiceClient(new DelegateAuthenticationProvider(
+                                        async (requestMessage) =>
+                                        {
+                                            AuthenticationResult result;
+                                            try
+                                            {
+                                                var accounts = await App.GetAccountsAsync();
+                                                result = await App.AcquireTokenSilent(Scopes, accounts.FirstOrDefault())
+                                                 .ExecuteAsync();
+                                            }
+                                            catch (MsalUiRequiredException ex)
+                                            {
+                                                result = await App.AcquireTokenInteractive(Scopes)
+                                                 .WithClaims(ex.Claims)
+                                                 .ExecuteAsync();
+                                            }
+
+                                            requestMessage.Headers.Authorization = new AuthenticationHeaderValue("bearer", result.AccessToken);
+
+                                        }));
+            return graph;
+        }
+    }
+}
